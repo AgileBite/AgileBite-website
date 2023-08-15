0c601f985d37bb62fd4368d9437be81f18f92bf6
AgileBite <agilebite.3@gmail.com>
2023-08-15 19:56:59 +0530

    .

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     
     # Run the app with the specified host and port
-    app.run(host="0.0.0.0", port=port)
+    application.run(host="0.0.0.0", port=port)
